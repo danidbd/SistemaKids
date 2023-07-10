f5bc2cdf952815185f5c9b7f8b168c92642178a2
daiel <dbrumcassimiro@gmail.com>
2023-07-10 17:53:23 -0300

    metodos de tempo

--- src/Crianca.java
+++ src/Crianca.java
@@ -3,14 +3,14 @@ import java.util.Map;
 
 public class Crianca {
     private String Nome;
-    private Date DataNascimento;
+    private Date dataNascimento;
     private  String ProblemaSaude;
     private String Alergia;
     private Map <String, String> horarioremedio;
 
     public Crianca(String nome, Date dataNascimento, String problemaSaude, String alergia, Map<String, String> horarioremedio) {
         Nome = nome;
-        DataNascimento = dataNascimento;
+        dataNascimento = dataNascimento;
         ProblemaSaude = problemaSaude;
         Alergia = alergia;
         this.horarioremedio = horarioremedio;
@@ -18,6 +18,26 @@ public class Crianca {
 
     public String getNome() {
         return Nome;
+    }
+
+    public int calcularTempoPermanencia(Date dataSaida) {
+        long diffMillis = dataSaida.getTime() - dataNascimento.getTime();
+        long diffMinutes = diffMillis / (60 * 1000);
+        return (int) diffMinutes;
+    }
+
+    public double calcularValorPermanencia(int tempoPermanencia){
+        int tempoMinimo = 30;
+        double valorMinimo = 20.0;
+        double valorPorMinito = 0.65;
+
+        if (tempoPermanencia <= tempoMinimo){
+            return valorMinimo;
+        }
+        else {
+            double valorExedente = (tempoPermanencia - tempoPermanencia)  * valorMinimo;
+            return valorMinimo + valorExedente;
+        }
     }
 
 }
